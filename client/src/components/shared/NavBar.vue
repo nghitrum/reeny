227e771983add008d29b618aa30fbfcb456d676b
joelmu <joel.muittari@gmail.com>
2018-03-21 15:44:29 +0200

    Navbar

--- client/src/components/shared/NavBar.vue
+++ client/src/components/shared/NavBar.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>This is a NavBar</h1>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <img src="../../assets/logo.png" class="p-1 mr-3" alt="Reeny">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        <div class="form-inline mr-3">
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <div class="btn-group mr-2" role="group">
+              <vue-login></vue-login>
+              <vue-register></vue-register>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
+import Login from './Login-button'
+import Register from './Register-button'
+
 export default {
+  components: {
+    'vue-login': Login,
+    'vue-register': Register
+  }
 
 }
 </script>
-
-<style>
+<style scoped>
 
 </style>
---
+++ client/src/components/shared/Register-button.vue
@@ -0,0 +1,3 @@
+<template>
+  <button type="button" class="btn btn-dark">Register</button>
+</template>
