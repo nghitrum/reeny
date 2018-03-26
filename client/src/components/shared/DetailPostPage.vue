4a15b1e0a4a7ed6ade4628b5c5b3aa45b5d339db
joelmu <joel.muittari@gmail.com>
2018-03-26 11:23:24 +0300

    page

---
+++ client/src/components/shared/DetailPostPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="heading-container">
+      <div>
+        <div class="votes">
+          {{post.rating}}
+        </div>
+        <div>
+          <div class="title">
+            {{post.title}}
+          </div>
+          <div>
+            posted by {{post.user}}
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>{{post.rating}} </div>
+        <div>{{post.createdAt}} </div>
+      </div>
+    </div>
+
+    <div class="picture-container">{{post.images}} </div>
+    <div class="text-body">{{post.content}} </div>
+    <div>{{post.tags}} </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+const queryPost = gql`
+  query Post {
+    post(id:"5ab8a712fa0c443628c8db64") {
+      title
+      content
+      tags
+      rating
+      createdAt
+      user
+    }
+  }
+`
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  apollo: {
+    // fetch all users
+    users: {
+      query: queryPost
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
