9af0695a202e44c2ea2793cd2c9808e29b9c9c6f
Dmitrii Stetcko <dimastetsko@gmail.com>
2018-04-04 17:10:53 +0300

    #32 Frontend done

--- client/src/components/shared/Voting.vue
+++ client/src/components/shared/Voting.vue
@@ -1,54 +1,75 @@
 <template>
-	<div class="rating">
-		<i class="fas fa-long-arrow-alt-up" @click="upVote"></i>
-		<h3>{{post.upVote - post.downVote}}</h3>
-		<i class="fas fa-long-arrow-alt-down" @click="downVote"></i>
-	</div>
+  <div class="rating">
+    <span class="clickable-icon" @click="upVote">
+      <i class="fas fa-long-arrow-alt-up"></i>
+    </span>
+    <h3>{{upVoteCount - downVoteCount}}</h3>
+    <span class="clickable-icon" @click="downVote">
+      <i class="fas fa-long-arrow-alt-down"></i>
+    </span>
+  </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
-
 const upVoteMutation = gql`
-  mutation upVoteMutation($username: String, $password: String) {
-    postUpVote(post: post.id) {
-      upVote
-    }
+mutation upVoteMutation($id: ID) {
+  postUpVote(post: $id){
+    upVote
   }
+}
 `
-
 const downVoteMutation = gql`
-  mutation downVoteMutation($username: String, $password: String) {
-    postDownVote(post: post.id) {
-      downVote
-    }
+mutation downVoteMutation($id: ID) {
+  postDownVote(post: $id){
+    downVote
   }
+}
 `
 export default {
-	data () {
-		return {
-			posts: []
-		}
-	},
-	methods: {
-		upVote () {
-			this.apollo.mutate({
-				mutation: postDownVote
-			})
-		}
-	},
-	downVote () {
-		this.apollo.mutate({
-			mutation: postDownVote
-		})
-	}
+  data () {
+    return {
+      postId: this.id,
+      upVoteCount: this.inputUpVote,
+      downVoteCount: this.inputDownVote
+    }
+  },
+  props: ['id', 'inputUpVote', 'inputDownVote'],
+  methods: {
+    upVote () {
+      this.$apollo.mutate({
+        mutation: upVoteMutation,
+        variables: {
+          id: this.postId
+        }
+      }).then(res => {
+        this.upVoteCount++ // = res.data.postUpVote.upVote
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    downVote () {
+      this.$apollo.mutate({
+        mutation: downVoteMutation,
+        variables: {
+          id: this.postId
+        }
+      }).then(res => {
+        this.downVoteCount++ // = res.data.postDownVote.downVote
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
-
 </script>
 
 <style scoped>
 .rating {
   width: 50%;
   text-align: center;
+}
+.clickable-icon {
+  cursor: pointer;
 }
 </style>
